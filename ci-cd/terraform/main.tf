1a8f740eaf0724f7779f4d0b9cdaccc5e511a779
Melquíades Maciel <ter_melquiades.macie@tempoassist.com.br>
2024-04-07 01:49:51 +0100

    feat: adicionado ec2 exemplo

---
+++ ci-cd/terraform/main.tf
@@ -0,0 +1,108 @@
+provider "aws" {
+  region = local.region
+  default_tags {
+    tags = {
+      Project    = "iac-ci-cd"
+      Terraform  = "true"
+      CI         = "true"
+      Repository = "https://github.com/clouderizando/iac-ci-cd"
+    }
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  name   = "iac-ci-cd-${basename(path.cwd)}"
+  region = "us-east-1"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Name = local.name
+  }
+}
+
+################################################################################
+# EC2 Module
+################################################################################
+
+module "ec2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.6.1"
+
+  name = local.name
+
+  subnet_id              = element(module.vpc.intra_subnets, 0)
+  vpc_security_group_ids = [module.security_group_instance.security_group_id]
+
+  instance_type               = "t2.micro"
+  create_iam_instance_profile = true
+  iam_role_description        = "IAM role for EC2 instance"
+  iam_role_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  tags = local.tags
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs           = local.azs
+  intra_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+
+  tags = local.tags
+}
+
+module "security_group_instance" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${local.name}-ec2"
+  description = "Security Group for EC2 Instance Egress"
+
+  vpc_id = module.vpc.vpc_id
+
+  egress_rules = ["https-443-tcp"]
+
+  tags = local.tags
+}
+
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "~> 5.0"
+
+  vpc_id = module.vpc.vpc_id
+
+  endpoints = { for service in toset(["ssm", "ssmmessages", "ec2messages"]) :
+    replace(service, ".", "_") =>
+    {
+      service             = service
+      subnet_ids          = module.vpc.intra_subnets
+      private_dns_enabled = true
+      tags                = { Name = "${local.name}-${service}" }
+    }
+  }
+
+  create_security_group      = true
+  security_group_name_prefix = "${local.name}-vpc-endpoints-"
+  security_group_description = "VPC endpoint security group"
+  security_group_rules = {
+    ingress_https = {
+      description = "HTTPS from subnets"
+      cidr_blocks = module.vpc.intra_subnets_cidr_blocks
+    }
+  }
+
+  tags = local.tags
+}
